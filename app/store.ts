e817763a18c79706b37950f0f8e41710286a06f6
SiriusShift <lagmanmarquez@gmail.com>
2025-02-18 23:00:18 +0800

    signup logic, add reducer, api

--- app/store.ts
+++ app/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from "../features/api";
+import userReducer from '../features/authentication/reducer/loginSlice'; // Import the user slice reducer
+
 const store = configureStore({
   reducer: {
     // Add the RTK Query reducer
+    user: userReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
--- features/api.ts
+++ features/api.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { BASE_URL } from "@env";
+import { BASE_URL } from "@env";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
